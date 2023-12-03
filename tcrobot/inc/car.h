a45f9f226ec81d42cf692c8f53deee1462a75eb5
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-12-03 12:31:30 -0300

    funcoes aperfeicoadas e controle de pwm tornado em uma funcao universal

--- tcrobot/inc/car.h
+++ tcrobot/inc/car.h
@@ -5,13 +5,21 @@
 #ifndef BBB_TCROBOT_CAR_H
 #define BBB_TCROBOT_CAR_H
 
+#include <stdio.h>
+#include "pwm.h"
+#include "file.h"
+
+/** SYSTEM PATHING DEFINITION **/
+
+// REFLEXIVE INFRARED SENSORS
 #define LEFT_SENSOR_PATH "/sys/class/gpio/gpio70/value"
 #define RIGHT_SENSOR_PATH "/sys/class/gpio/gpio71/value"
 #define CENTER_SENSOR_PATH "/sys/class/gpio/gpio72/value"
 
-#define LEFT_MOTOR_PWM_PATH "/sys/class/pwm/pwmchip0/pwm0/duty_cycle"
-#define RIGHT_MOTOR_PWM_PATH "/sys/class/pwm/pwmchip0/pwm1/duty_cycle"
+// DC MOTORS
+#define LEFT_MOTOR_PWM_PATH "/sys/class/pwm/pwmchip0/pwm0"
+#define RIGHT_MOTOR_PWM_PATH "/sys/class/pwm/pwmchip0/pwm1"
 
-#define PWM_PERIOD 200000
+#define PWM_PERIOD 200000 // WHEELs DC MOTORS PWM (5 KHz)
 
 #endif //BBB_TCROBOT_CAR_H
