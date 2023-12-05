b14362364c7df8a2ddb8764b4e113ee76b5a2e70
guiaf04 <guilhermearaujo@alu.ufc.br>
2023-12-04 21:54:30 -0300

    movimento do sg90 baseado no ângulo

--- tcrobot/inc/sg90.h
+++ tcrobot/inc/sg90.h
@@ -2,3 +2,13 @@
  * SERVOS SG90 LIBRARY
  * This library control SG90 servos movement
  */
+
+#ifndef SG90_H
+#define SG90_H
+
+#include <fcntl.h>
+#include <unistd.h>
+
+void servo_motor(char pin[], int angle);
+
+#endif // SG90_H
--- tcrobot/src/sg90.c
+++ tcrobot/src/sg90.c
@@ -1,1 +1,34 @@
 // todo
+#include "../inc/pwm.h"
+
+void servo_motor(char pin[], int angle){
+
+    enable_pwm(pin);
+
+    char pwm_path[256];
+    
+    sprintf(pwm_path, "%s/period", get_pwm_path(pin));
+
+    int fd = open(pwm_path, O_WRONLY);
+    if (fd < 0){
+        printf("Error opening period file\n");
+        return;
+    }
+
+    write(fd, "20000000", 8);
+    close(fd);
+
+    sprintf(pwm_path, "%s/duty_cycle", get_pwm_path(pin));
+
+    fd = open(pwm_path, O_WRONLY);
+    if (fd < 0){
+        printf("Error opening dutty cicle file\n");
+        return;
+    }
+    double dutty_cycle =  200000 * ((angle * 1.0 / 18) + 2.5);
+    char dutty_cycle_str[256];
+    
+    sprintf(dutty_cycle_str, "%d", (int)dutty_cycle);
+    write(fd, dutty_cycle_str, strlen(dutty_cycle_str));
+    close(fd);
+}
